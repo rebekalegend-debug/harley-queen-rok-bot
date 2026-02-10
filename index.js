f1aa63860adb14fad1e8f12f9c92e1671cd10b04
rebekalegend-debug <rebekalegend@gmail.com>
2026-02-10 03:01:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ import { setupAooMgeEvent } from "./modules/aoomgeevent.js";
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
   ],
